fe7e9c8f5dc3f3ce3d45be59da0a7cca3fb68272
zluetj <zluetj@WS-ZLUETJ.bbcnet-zh.local>
2018-02-22 10:04:00 +0100

    Import of HomeMenu

--- OnlineShop/src/ch/bbcag/onlineShop/view/DesignPanel.java
+++ OnlineShop/src/ch/bbcag/onlineShop/view/DesignPanel.java
@@ -6,7 +6,6 @@ import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
