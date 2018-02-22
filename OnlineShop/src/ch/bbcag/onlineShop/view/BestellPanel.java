56470e578a664578425aa7852b16b35fa2d8c06d
zhoefs <zhoefs@WS-ZHOEFS.bbcnet-zh.local>
2018-02-22 15:19:36 +0100

    ...

--- OnlineShop/src/ch/bbcag/onlineShop/view/BestellPanel.java
+++ OnlineShop/src/ch/bbcag/onlineShop/view/BestellPanel.java
@@ -1,6 +1,5 @@
 package ch.bbcag.onlineShop.view;
 
-import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
@@ -9,7 +8,6 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
