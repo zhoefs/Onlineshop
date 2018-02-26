d91556c2c2263cdb75706b245c6103e953626f7d
zhoefs <zhoefs@WS-ZHOEFS.bbcnet-zh.local>
2018-02-26 11:08:00 +0100

    ...

---
+++ OnlineShop/src/ch/bbcag/onlineShop/view/HomeMenuTest.java
@@ -0,0 +1,23 @@
+package ch.bbcag.onlineShop.view;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class HomeMenuTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+}
